7c1f5d6d1c6d5dee90249787fddfd769a8eda3c2
gubalo <gbanuelos94@gmail.com>
2021-05-17 23:28:05 -0500

    Dev
    LayoutService

--- Symas-Angular/ClientApp/src/app/counter/counter.component.ts
+++ Symas-Angular/ClientApp/src/app/counter/counter.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LayoutService } from '../services/layouts/layouts.service';
 
 @Component({
   selector: 'app-counter-component',
@@ -6,6 +7,10 @@ import { Component } from '@angular/core';
 })
 export class CounterComponent {
   public currentCount = 0;
+
+  constructor(public layoutService:LayoutService){
+    this.layoutService.title="Counter";
+  }
 
   public incrementCounter() {
     this.currentCount++;
